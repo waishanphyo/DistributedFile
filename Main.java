b2559f6c41d915e8dfa73fda629120e2ab72435d
Wai Shan Phyo <hahawah538@gmail.com>
2024-09-03 19:28:24 +0630

    First

---
+++ Main.java
@@ -0,0 +1,11 @@
+import CONTROLLER.RegisterController;
+import VIEW.MainPage;
+import VIEW.RegisterForm;
+
+public class Main {
+    public static void main(String[] args){
+      //  RegisterForm view=new RegisterForm();
+      //  RegisterController controller=new RegisterController(view);
+    }
+
+}
